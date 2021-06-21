28a4fdc4690d9e1496833ef3f1e07096033e4fb5
yamlaksira <yamlaksirasahilu@gmail.com>
2021-06-21 09:31:21 -0700

    lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-  char c;
+char c;
 
-  for (c = 'z'; c >= 'a'; c--)
-    {
-      putchar(c);
-    }
-  putchar('\n');
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
 
-  return (0);
+return (0);
 }
