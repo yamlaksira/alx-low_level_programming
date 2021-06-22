a8be2c7f8976aa74db0fa4276a88f32e60991d4a
yamlaksira <yamlaksirasahilu@gmail.com>
2021-06-22 07:25:05 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-  char holberton[] = "Holberton";
-  int i = 0;
+char holberton[] = "Holberton";
+int i = 0;
 
-  while (holberton[i] != '\0')
-    {
-      _putchar(holberton[i]);
-      i++;
-    }
-  _putchar('\n');
+while (holberton[i] != '\0')
+{
+_putchar(holberton[i]);
+i++;
+}
+_putchar('\n');
 
-  return (0);
+return (0);
 }
