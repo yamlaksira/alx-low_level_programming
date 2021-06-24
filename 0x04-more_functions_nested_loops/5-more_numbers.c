691a45815b6e29fc85015be8507e13a85574dde5
yamlaksira <yamlaksirasahilu@gmail.com>
2021-06-24 10:08:07 -0700

    triangle

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * more_numbers - print 10 times the numbers from 0 to 14
+ * more_numbers - print 10 times the numbers, from 0 to 14
  * Description: can only use _putchar 3 times
  */
 void more_numbers(void)
@@ -12,7 +12,7 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 15; j++)
 {
 if (j >= 10)
-	
+
 _putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 }
