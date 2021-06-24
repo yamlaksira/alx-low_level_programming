97fb2bd26eec51e4e6eaafa7c0b4b4e74e704372
yamlaksira <yamlaksirasahilu@gmail.com>
2021-06-24 07:46:27 -0700

    is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+  return (c >= 'A' && c <= 'Z');
+}
