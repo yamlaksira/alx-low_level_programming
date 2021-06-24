826fee7b03bd2544273def6d964e96f0b3dfcd6f
yamlaksira <yamlaksirasahilu@gmail.com>
2021-06-24 13:14:21 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-printf("Infinite loop incoming: (\n"));
+printf("Infinite loop incoming: (\n");
 
 i = 0;
 
@@ -18,7 +18,7 @@ i = 0;
 /* putchar(i); */
 /* } */
 
-printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\0/\n");
 
 return (0);
 }
