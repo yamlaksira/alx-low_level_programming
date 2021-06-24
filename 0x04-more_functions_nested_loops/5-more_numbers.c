dad4bac52ee369e193758c030fe306a056eac364
yamlaksira <yamlaksirasahilu@gmail.com>
2021-06-24 08:36:05 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: can only use _putchar 3 times
+ */
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+if (j >= 10)
+	
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
+}
