42c8c6d3f168afadb37c0dccadc168a03bb683fd
yamlaksira <yamlaksirasahilu@gmail.com>
2021-06-24 08:03:35 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - multiple two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+int c;
+
+c = a * b;
+
+return (c);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 # 0x04-more_functions_nested_loops
 # 0-isupper.c
 # 1-isdigit.c
+# 2-mul.c
